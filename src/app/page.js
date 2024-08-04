eaa75bdd91f5c29588750d5d38583cf1bb4c9467
Purchase Metaverk <satyendra.kumar@metaverk.io>
2024-08-04 09:40:04 +0530

    ui update

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          sa Get started by editing&nbsp;
+           satyendra kumar ss&nbsp;
           <code className={styles.code}>src/app/page.js</code>
         </p>
         <div>
